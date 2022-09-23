44d74d995dab2ad2e8d72f9a8df1c009faef8266
Ochanda Charles Otieno <65234472+lyricalcoder16@users.noreply.github.com>
2022-09-23 12:49:10 +0300

    simple C++ calculator

---
+++ Simple calc/Simple calc.cpp
@@ -0,0 +1,12 @@
+#include <iostream.h>
+using namespace std;
+int main(){
+    int x,y;
+    int sum;
+    cout <<"Type a number";
+    cin << x;
+    cout<< "Type another number";
+    cin <<y;
+    int sum = x+y;
+    cout<< "the sum is"sum;
+}w
